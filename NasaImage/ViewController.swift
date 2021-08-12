85e6a94773d9598662d52571d237495a9d014955
Wilhelm Thieme <will.thieme@sogeti.com>
2021-08-12 18:55:54 +0200

    Initial Commit

---
+++ NasaImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NasaImage
+//
+//  Created by Wilhelm Thieme on 12/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

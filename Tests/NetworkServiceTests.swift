20cf3a9b5c6229a593249e28312b11e101f4799d
Wilhelm Thieme <will.thieme@sogeti.com>
2021-08-13 17:55:30 +0200

    fixed networktests

--- Tests/NetworkServiceTests.swift
+++ Tests/NetworkServiceTests.swift
@@ -17,106 +17,90 @@ class NetworkServiceTests: XCTestCase {
     }()
     
     func testSuccess() {
-        var calledBack = false
+        let expectation = XCTestExpectation(description: "Web call")
         let networking = NetworkServiceImpl()
         networking.session = session
         networking.reachability = ReachabilityServiceMock(true)
         URLProtocolMock.requestHandler = { _ in
             return (200, Util.validSearch)
         }
-        _ = networking.get(urlString: Util.invalidUrl) { data, error in
-            calledBack = true
-            XCTAssertNil(data)
-            XCTAssertNotNil(error)
+        _ = networking.get(urlString: Util.validUrl) { data, error in
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            expectation.fulfill()
         }
-        XCTAssertTrue(calledBack)
+        wait(for: [expectation], timeout: 5)
     }
     
     func testError() {
-        var calledBack = false
+        let expectation = XCTestExpectation(description: "Web call")
         let networking = NetworkServiceImpl()
         networking.session = session
         networking.reachability = ReachabilityServiceMock(true)
         URLProtocolMock.requestHandler = { _ in
             throw Util.testError
         }
-        _ = networking.get(urlString: Util.invalidUrl) { data, error in
-            calledBack = true
+        _ = networking.get(urlString: Util.validUrl) { data, error in
             XCTAssertNil(data)
             XCTAssertNotNil(error)
+            expectation.fulfill()
         }
-        XCTAssertTrue(calledBack)
+        wait(for: [expectation], timeout: 5)
     }
     
     func testBadStatus() {
-        var calledBack = false
+        let expectation = XCTestExpectation(description: "Web call")
         let networking = NetworkServiceImpl()
         networking.session = session
         networking.reachability = ReachabilityServiceMock(true)
         URLProtocolMock.requestHandler = { _ in
             return (300, nil)
         }
-        _ = networking.get(urlString: Util.invalidUrl) { data, error in
-            calledBack = true
+        _ = networking.get(urlString: Util.validUrl) { data, error in
             XCTAssertNil(data)
             XCTAssertNotNil(error)
+            expectation.fulfill()
         }
-        XCTAssertTrue(calledBack)
+        wait(for: [expectation], timeout: 5)
     }
     
     func testBadStatusWithValidData() {
-        var calledBack = false
+        let expectation = XCTestExpectation(description: "Web call")
         let networking = NetworkServiceImpl()
         networking.session = session
         networking.reachability = ReachabilityServiceMock(true)
         URLProtocolMock.requestHandler = { _ in
             return (300, Util.validSearch)
         }
-        _ = networking.get(urlString: Util.invalidUrl) { data, error in
-            calledBack = true
+        _ = networking.get(urlString: Util.validUrl) { data, error in
             XCTAssertNil(data)
             XCTAssertNotNil(error)
+            expectation.fulfill()
         }
-        XCTAssertTrue(calledBack)
-    }
-    
-    func testEmptyBody() {
-        var calledBack = false
-        let networking = NetworkServiceImpl()
-        networking.session = session
-        networking.reachability = ReachabilityServiceMock(true)
-        URLProtocolMock.requestHandler = { _ in
-            return (200, nil)
-        }
-        _ = networking.get(urlString: Util.invalidUrl) { data, error in
-            calledBack = true
-            XCTAssertNil(data)
-            XCTAssertNotNil(error)
-        }
-        XCTAssertTrue(calledBack)
+        wait(for: [expectation], timeout: 5)
     }
     
     func testInvalidUrl() {
-        var calledBack = false
+        let expectation = XCTestExpectation(description: "Web call")
         let networking = NetworkServiceImpl()
         networking.reachability = ReachabilityServiceMock(true)
         _ = networking.get(urlString: Util.invalidUrl) { data, error in
-            calledBack = true
             XCTAssertNil(data)
             XCTAssertNotNil(error)
+            expectation.fulfill()
         }
-        XCTAssertTrue(calledBack)
+        wait(for: [expectation], timeout: 5)
     }
     
     func testNetworkNotReachable() {
-        var calledBack = false
+        let expectation = XCTestExpectation(description: "Web call")
         let networking = NetworkServiceImpl()
         networking.reachability = ReachabilityServiceMock(false)
         _ = networking.get(urlString: Util.validUrl) { data, error in
-            calledBack = true
             XCTAssertNil(data)
             XCTAssertNotNil(error)
+            expectation.fulfill()
         }
-        XCTAssertTrue(calledBack)
+        wait(for: [expectation], timeout: 5)
     }
 }
